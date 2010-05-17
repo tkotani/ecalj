99e040eb7abdb960c068e9b8ebcbfa16f25e1365
Takao Kotani <takaokotani@yahoo.co.jp>
2010-05-17 16:48:51 +0900

    Modify rdsimg2.F, freeat.F sugw.F (deallocation error <--delw_2.2.py).
    TOOLS/FparserTools/ contains f2py/fparser related files (taken from
    f2py/fparser r58). 
    I gave a modification to TOOLS/FparserTools/f_calltree.py.
    ("If" block is not scanned well by fparser r58). So I think now 
    lm7K/CallCaller.sh works fine.
    
    fparser may be a little too complicated for our purpose
    (inheritance of base classes as Statement and BlockStamente and so).

--- lm7K/CallCaller.sh
+++ lm7K/CallCaller.sh
@@ -2,13 +2,13 @@
 echo ##############
 echo  'This generate a call-caller data set for fp/\*.F and subs/\*.F .'
 echo  'No arguments required. This drives ../TOOLS/f_calltree.py '
-echo  'HELP --> ../TOOLS/f_calltree.py --help, and read CallCaller.sh'
+echo  'HELP --> ../TOOLS/FparserTools/f_calltree.py --help, and read CallCaller.sh'
 echo
 echo  '--- Now generating a file 'callcaller.dat' ... It takes 1 minute or so!'
 echo
 echo ##############
 #../TOOLS/ANALYZE/analyze.py freeat fp/*.F subs/*.F slatsm/*.F gwd/*.F >callcaller.dat ; grep tree callcaller.dat > lmfa_tree
-../TOOLS/f_calltree.py lmv7.F lmfav7.F lmv7util.F fp/*.F subs/*.F >callcaller.dat 2>callcaller.err
+../TOOLS/FparserTools/f_calltree.py lmv7.F lmfav7.F lmv7util.F fp/*.F subs/*.F >callcaller.dat 2>callcaller.err
 #../TOOLS/f_calltree.py lmv7.F fp/*.F >callcaller.dat 2>callcaller.err
 egrep -e '(ERROR|Error)' callcaller.err
 echo
