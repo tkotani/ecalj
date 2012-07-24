122e27eb7c1ca37717ec417c0320d02d6e8401d1
takao <takao@TT4>
2012-07-25 00:14:33 +0900

    ctrlgen2.py Default is PWMODE=1
    I think PWMODE=11 is better for interpolation of self-energy 
    in the BZ. Considering the case of large cell, PWMODE=1 is 
    only slightly inefficient probably.

--- lm7K/ctrlgen2.py
+++ lm7K/ctrlgen2.py
@@ -864,10 +864,15 @@ tail = tail + "     XCFUN=" + xcfun_val + """
 		#    (subs/evxc.F had a problem when =2 if rho(up)=0 or rho(down)=0).
                 # =103 PBE-GGA
 
-      PWMODE=11 # 10: MTO basis only (LMTO) PW basis is not used.
-                # 11: APW+MTO        (PMT)
-                # 12: APW basis only (LAPW) MTO basis is not used.
-
+      PWMODE=1  # 0: MTO basis only (LMTO) PW basis is not used.
+                # 1: APW+MTO        (PMT)
+                # 2: APW basis only (LAPW) MTO basis is not used.
+                # NOTE: q-independent number of APW (determined at q=0).
+                #       This will be important for QSGW case.
+                #
+                # ===  +10 means q-dependent number of APW, that is, ===
+                # 11: PMT   
+                # 12: LAPW: 
       PWEMAX={pwemax} # (in Ry). When you use larger pwemax more than 5, be careful
                       # about overcompleteness. See GetStarted.
 """
