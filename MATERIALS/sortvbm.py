63cb36b3dcd75b0d9314eff43c2a47c129d16bec
takao <takao@TT4>
2013-05-25 16:07:31 +0900

    add syml files.

---
+++ MATERIALS/sortvbm.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+import re,sys,string,os,signal,time
+vbm  = open("VBM.dat",'rt').read().split('\n')
+vbmref= open("VBMref.dat",'rt').read().split('\n')
+
+vbmdat={}
+for line in vbm:
+    if line=='' : continue
+    if line[0]=='#': continue
+    if line[0]=='=': continue
+    line= re.sub('###','',line)
+    liner=line.split('/')
+    matid=liner[0].strip()
+    #print matid,liner[1]
+    vdat=liner[1].split('VBM=')[1]
+    vdat=vdat.strip().split(' ')
+    vbmdat[matid]=vdat[0]
+print vbmdat
+print vbmdat.keys()
+
+########################################
+for line in vbmref:
+    if line=='' : continue
+    if line[0]=='#': continue
+    if line[0]=='=': continue
+    dat=re.split(r'\s+',line)
+    mat = dat[0]
+    print mat,vbmdat[mat],dat[1],dat[2],dat[3],dat[4]
+
+sys.exit()
