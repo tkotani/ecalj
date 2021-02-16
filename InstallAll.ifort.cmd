7c6502fe47caed5a7e5dca3ddd8d76781d145103
Takao Kotani <takaokotani@gmail.com>
2021-02-16 20:38:14 +0900

    CMD38: Add CleanAll, InstallAll.ifort.cmd

---
+++ InstallAll.ifort.cmd
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+BINDIR=${HOME}/bin
+MATH='-mkl'
+
+function my_error() {
+    exit 1
+}
+
+###
+mkdir ${BINDIR}
+echo Going to install required binaries and scripts to ${BINDIR} !
+
+### make a link to getsyml
+ln -s  ${PWD}/GetSyml/getsyml ${BINDIR}/getsyml
+
+### viewvesta
+pushd .
+cd StructureTool/
+./makelink $BINDIR
+popd
+
+### 
+
+pushd .
+cd SRC/exec
+make PLATFORM=ifort LIBMATH="$MATH" FC=mpifort LK=mpifort ||my_error
+make PLATFORM=ifort BINDIR=$BINDIR  FC=mpifort LK=mpifort install ||my_error
+popd
+
+echo '=== goto test ==='
+cd SRC/TestInstall/
+make mpi_size=4 all
+
