cbdb2b5e21a255f7131d43611a0beaa6ebe505e8
Takao Kotani <takaokotani@gmail.com>
2022-05-11 11:51:21 +0900

    improve general format m_ftox

--- SRC/subroutines/m_ftox.f90
+++ SRC/subroutines/m_ftox.f90
@@ -1,7 +1,9 @@
-! General module for formatted write'
-! ftox: general format. usage write(6,ftox) foobar,xxx
-! ftom(drealx) truncate right-hand-side zeros. drealx can be real(8) arrays.
-! ftof(drealx,m) show m digits below period. m is option (six when no m)
+! General purpose formatted write module m_ftox
+
+! ftox: general format. usage: write(6,ftox) foobar,xxx
+! ftof(dfloat,m) 1234.123456 format. m digits below period. m is option (six when no m)
+! ftod(dfloat,m) 0.123456d+08 format.
+! ftom(dfloat)   truncate right-hand-side zeros. drealx can be real(8) arrays.
 !==============
 ! program test_ftox
 !   use m_ftox
@@ -21,25 +23,27 @@
 !>nnn2 1.240000 4.560000 117.123459
 !>nnn3 0.12D+01 0.46D+01 0.12D+03 0.46D+07 0.91D+05
 module m_FtoX
+  implicit none
   public:: ftof,ftod,ftom !ftom is removing right-end zeros below decimal point.
   character(11),public:: ftox='(*(g0,x))'
-  interface ftof
-     module procedure ftof,ftofv
+  interface ftof !123.456789 format
+     module procedure ftof,ftofv,ftoc,ftocv
   endinterface ftof
-  interface ftod
-     module procedure ftod,ftodv
+  
+  interface ftod !0.123456D+8 format
+     module procedure ftod,ftodv,ftocd,ftocdv
   endinterface ftod
-  interface ftom
+  
+  interface ftom !zero of righthand-side are truncated (mainly for inputs)
      module procedure ftom,ftomv
   endinterface ftom
-  private
 contains
   function ftodv(argv,ixx) result(farg)
     intent(in):: argv,ixx
     real(8):: argv(:)
-    integer::i,ix
     character(:),allocatable:: farg
     integer,optional:: ixx
+    integer::ix,i
     character(1000):: mmm
     ix=6
     if(present(ixx)) ix=ixx
@@ -52,10 +56,10 @@ contains
   function ftofv(argv,ixx) result(farg)
     intent(in):: argv,ixx
     real(8):: argv(:)
-    integer::i,ix
     character(:),allocatable:: farg
     integer,optional:: ixx
     character(1000):: mmm
+    integer::ix,i
     ix=6
     if(present(ixx)) ix=ixx
     write(mmm,"(*(g0,x))") (ftof(argv(i),ix),i=1,size(argv))
@@ -64,59 +68,118 @@ contains
     allocate(farg,source=mmm(1:len(trim(mmm))))
   end function ftofv
 
+  function ftocdv(argv,ixx) result(farg)
+    intent(in):: argv,ixx
+    complex(8):: argv(:)
+    character(:),allocatable:: farg
+    integer,optional:: ixx
+    character(1000):: mmm
+    integer:: i,ix
+    ix=6
+    if(present(ixx)) ix=ixx
+    write(mmm,"(*(g0,x))") (ftocd(argv(i),ix),i=1,size(argv))
+    mmm=adjustl(mmm)
+    if(allocated(farg)) deallocate(farg)
+    allocate(farg,source=mmm(1:len(trim(mmm))))
+  end function ftocdv
+
+  function ftocv(argv,ixx) result(farg)
+    intent(in):: argv,ixx
+    complex(8):: argv(:)
+    character(:),allocatable:: farg
+    integer,optional:: ixx
+    character(1000):: mmm
+    integer:: i,ix
+    ix=6
+    if(present(ixx)) ix=ixx
+    write(mmm,"(*(g0,x))") (ftoc(argv(i),ix),i=1,size(argv))
+    mmm=adjustl(mmm)
+    if(allocated(farg)) deallocate(farg)
+    allocate(farg,source=mmm(1:len(trim(mmm))))
+  end function ftocv
+
   function ftomv(argv,ixx) result(farg)
     intent(in):: argv,ixx
+    integer:: i,ix
     real(8):: argv(:)
-    integer::i,ix
     character(:),allocatable:: farg
     integer,optional:: ixx
     character(1000):: mmm
     ix=6
     if(present(ixx)) ix=ixx
-    write(mmm,"(*(g0,x))") (ftom(argv(i),ix),i=1,size(argv))
+!    print *,'ftomv',size(argv)
+    write(mmm,"(*(g0,x))") (ftom(argv(i)),i=1,size(argv))
     mmm=adjustl(mmm)
     if(allocated(farg)) deallocate(farg)
     allocate(farg,source=mmm(1:len(trim(mmm))))
   end function ftomv
-
+  
+!ccccccccccccccccccccccccccccccccccccccccccccccccccc
   function ftof(arg,ixx) result(farg)
     intent(in)::arg,ixx
     real(8):: arg
-    character(:),allocatable:: farg
-    character(32):: mmm,fmt
-    integer::lsize,ix
     integer,optional:: ixx
+    character(:),allocatable:: farg
+    integer::ix
     ix=6
     if(present(ixx)) ix=ixx
-    write(mmm,"("//"f32."//charnum3(ix)//")") arg
-    mmm=adjustl(mmm)
-    if(allocated(farg)) deallocate(farg)
-    allocate(farg,source=mmm(1:len(trim(mmm))))
+    farg = fwww("("//"f32."//charnum3(ix)//")",arg)
   end function ftof
   !
   function ftod(arg,ixx) result(farg)
     intent(in)::arg,ixx
     real(8):: arg
-    character(:),allocatable:: farg
-    character(32):: mmm
-    integer::lsize,ix
     integer,optional:: ixx
+    character(:),allocatable:: farg
+    integer::ix
     ix=6
     if(present(ixx)) ix=ixx
-    write(mmm,"(d32."//charnum3(ix)//")") arg
-    mmm  =adjustl(mmm)
+    farg = fwww("("//"d32."//charnum3(ix)//")",arg)
+  end function ftod
+  !  
+  function ftoc(arg,ixx) result(farg)
+    intent(in)::arg,ixx
+    complex(8):: arg
+    integer,optional:: ixx
+    character(:),allocatable:: farg
+    integer::ix
+    ix=6
+    if(present(ixx)) ix=ixx
+    farg="( "//ftof(dreal(arg),ix)//" , "//ftof(dimag(arg),ix)//" )"
+  end function ftoc
+  !  
+  function ftocd(arg,ixx) result(farg)
+    intent(in)::arg,ixx
+    complex(8):: arg
+    integer,optional:: ixx
+    character(:),allocatable:: farg
+    integer::ix
+    ix=6
+    if(present(ixx)) ix=ixx
+    farg="( "//ftod(dreal(arg),ix)//" , "//ftod(dimag(arg),ix)//" )"
+  end function ftocd
+  !  
+  function fwww(fmt,arg) result(farg)
+    intent(in)::fmt,arg
+    real(8):: arg
+    character(32):: mmm
+    character(*):: fmt
+    character(:),allocatable:: farg
+    write(mmm,fmt) arg
+    mmm=adjustl(mmm)
     if(allocated(farg)) deallocate(farg)
     allocate(farg,source=mmm(1:len(trim(mmm))))
-  end function ftod
+  end function fwww
   !  
   function ftom(arg,ixx) result(farg) !arg =3.45600000 is '3.45', trucates to rightside zeros'
     intent(in)::arg,ixx
     real(8):: arg
     character(:),allocatable:: farg
     character(32):: mmm,fmt
-    integer::lsize,ix,i,j
     integer,optional:: ixx
-    write(mmm,"(*(g0,x))")ftof(arg,12)
+    integer:: i,j
+    write(mmm,"(*(g0,x))")ftof(arg,16)
+!    print *,'vvvxxx=',trim(mmm)
     j=len(trim(mmm))
     do i=len(mmm),1,-1
        if(mmm(i:i)==' ') cycle
@@ -131,23 +194,26 @@ contains
     if(allocated(farg)) deallocate(farg)
     allocate(farg,source=mmm(1:j))
   end function ftom
-
-  
+ 
   character(3) function charnum3(num)
     integer(4) ::num
     charnum3=char(48+mod(num/100,10))//char(48+mod(num/10,10))//char(48+mod(num,10))
   end function charnum3
 end module m_FtoX
 
-! program test_ftox
-!   use m_ftox
-!   real(8):: w1,w2,aaa(5)
-!   character(12):: fff
-!   n=-3132922
-!   w1=12245.807807807d0
-!   w2=-243.901901901d0
-!   aaa=[1.24,4.56,117.1234567,4566666.9,90808.]
-!   write(6,ftox)'nnn1',n,'aaa',ftof(w1),'bbb',ftof(w1,5),'bbb',ftod(w2,12)
-!   write(6,ftox)'nnn2',ftof(aaa(1:3))
-!   write(6,ftox)'nnn3',ftod(aaa,2)
-! end program test_ftoX
+ ! program test_ftox
+ !   use m_ftox
+ !   real(8):: w1,w2,aaa(5)
+ !   complex(8):: bbb(5)
+ !   character(12):: fff
+ !   n=-3132922
+ !   w1=12245.807807807d0
+ !   w2=-243.901901901d0
+ !   aaa=[1.24,4.56,117.1234567,4566666.9,90808.]
+ !   bbb=[1.24,4.56,117.1234567,4566666.9,90808.]
+ !   write(6,ftox)'nnn1',n,'aaa',ftof(w1),'bbb',ftof(w1,5),'bbb',ftod(w2,12)
+ !   write(6,ftox)'nnn2',ftof(aaa)
+ !   write(6,ftox)'nnn2x',ftof(bbb)
+ !   write(6,ftox)'nnn3',ftod(aaa,2)
+ !   write(6,ftox)'nnn3mmm',ftom(aaa(1:3))
+ ! end program test_ftoX
