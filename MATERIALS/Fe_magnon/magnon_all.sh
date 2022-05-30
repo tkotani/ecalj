c38ee6bbe0cc6d54c47a884c567bc5dee21601fa
Takao Kotani <takaokotani@gmail.com>
2022-05-30 17:09:46 +0900

    Fe_magnon improved

--- MATERIALS/Fe_magnon/magnon_all.sh
+++ MATERIALS/Fe_magnon/magnon_all.sh
@@ -5,13 +5,11 @@ NSLOTS=4
 lmfa fe >& llmfa
 mpirun -np $NSLOTS lmf-MPIK fe >&llmf
 # ### 1. band calculation and create MLWFs
-cp GWinput_for_MLWF GWinput
 job_band $MATERIAL -np $NSLOTS NoGnuplot # &> job_band.log
 genMLWF_vw $MATERIAL -np $NSLOTS # &> genmlwf_vw.log
 
 ### 2. magnon calculation
 ### If we implement more detailed k, change n1n2n3 in "GWinput_for_magnon".
-cp GWinput_for_magnon GWinput
 epsPP_magnon_chipm_mpi -np $NSLOTS $MATERIAL
 
 ### 3. plotting Stoner and Magnon (eps output) 
