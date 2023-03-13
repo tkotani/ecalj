75a2198d4a4a9fc05aafb64eca0e228c8166488d
Takao Kotani <takaokotani@gmail.com>
2023-03-13 10:34:38 +0900

    make ok! cleaning rlocbl

--- SRC/subroutines/rlocbl.f90
+++ SRC/subroutines/rlocbl.f90
@@ -2,146 +2,24 @@ module m_rlocbl
   public rlocbl 
   private
 contains
-!   subroutine prlcb2(job,ia,nkaph,nlmha,kmax,nlma,isp,cPkL,nlmto,evec,ewgt,evl, qhh,qhp)
-!     use m_orbl,only: Orblib,ktab,ltab,offl,norb, ntab,blks
-!     !- Add one and two-center terms to density coeffs
-!     ! ----------------------------------------------------------------------
-!     !i Inputs
-!     !i   job   :0 accumulate local density-matrix
-!     !i         :1 accumulate local density-matrix weighted by energy
-!     !i   ia    :site of augmentation
-!     !i   nkaph :dimensions qhh,qhp
-!     !i   nlmha :dimensions qhh,qhp
-!     !i   kmax  :polynomial cutoff
-!     !i   nlma  :augmentation L-cutoff
-!     !i   isp   :spin channel
-!     !i   cPkL  :PkL expansion eigenvector at site ia.
-!     !i   nlmto :dimension of lmto component of basis
-!     !i   evec  :eigenvector
-!     !i   ewgt  :eigenvector weight
-!     !i   evl   :energy weight (job=1)
-!     !o Outputs
-!     !o   qhh   :one-center density-matrix for PkL expansion (job=0)
-!     !o         :energy-weighted matrix (job=1)
-!     !o   qhp   :two-center density-matrix for PkL expansion (job=0)
-!     !o         :energy-weighted matrix (job=1)
-!     !r Remarks
-!     !u Updates
-!     !u   05 Jul 08 (T. Kotani)
-!     !u             Option to accumulate energy-weighted output density
-!     !u   27 Aug 01 Extended to local orbitals.
-!     ! ----------------------------------------------------------------------
-!     implicit none
-!     integer :: job,ia,kmax,nkaph,isp,nlmto,nlma,nlmha
-!     double precision :: qhh(nkaph,nkaph,nlmha,nlmha,isp),qhp(nkaph,0:kmax,nlmha,nlma,isp),ewgt
-!     real(8),optional::evl
-!     double complex evec(nlmto),cPkL(0:kmax,nlma)
-!     integer :: i1,i2,ilm1,ilm2,ilma,io1,io2,iq,k,ik1,ik2,l1,l2,n0,nkap0,nlm11,nlm12,nlm21,nlm22
-!     parameter (n0=10,nkap0=3)
-! !    integer ::  blks(n0*nkap0),ntab(n0*nkap0)! orb,ltab(n0*nkap0),ktab(n0*nkap0),offl(n0*nkap0),
-!     double precision :: xx
-!     if (nlmto == 0) return
-!     call tcn('prlcb2')
-!     ! --- Loop over all orbitals centered at site ia, incl. local orbs ---
-!     call orblib(ia)! norb,ltab,ktab,offl
-!     !     Block into groups of consecutive l
-! !    call gtbsl4(ia) !1(4,norb,ltab,ktab,xx,xx,ntab,blks)
-!     do io1 = 1, norb
-!        l1  = ltab(io1)
-!        ik1 = ktab(io1)
-!        nlm11 = l1**2+1
-!        nlm12 = nlm11 + blks(io1)-1
-!        i1 = offl(io1) !  i1 = hamiltonian offset for first orbital in block
-!        do  ilm1 = nlm11, nlm12
-!           i1 = i1+1
-!           !     ... Accumulate products H*Pkl
-!           !          do  iq = 1, numq
-!           if (job == 0) then
-!              do  k = 0, kmax
-!                 do  ilma = 1, nlma
-!                    qhp(ik1,k,ilm1,ilma,isp)= qhp(ik1,k,ilm1,ilma,isp) &
-!                         + 2d0*dconjg(evec(i1))*cPkL(k,ilma)*ewgt !(iq)
-!                 enddo
-!              enddo
-!           else
-!              do  k = 0, kmax
-!                 qhp(ik1,k,ilm1,ilm1,isp)= qhp(ik1,k,ilm1,ilm1,isp) &
-!                      + evl*2d0*dconjg(evec(i1))*cPkL(k,ilm1)*ewgt !(iq)
-!              enddo
-!           endif
-!           !          enddo
-!           !     ... Accumulate products H*H
-!           do  io2 = 1, norb
-!              l2  = ltab(io2)
-!              ik2 = ktab(io2)
-!              nlm21 = l2**2+1
-!              nlm22 = nlm21 + blks(io2)-1
-!              i2 = offl(io2) 
-!              do  ilm2 = nlm21, nlm22
-!                 i2 = i2+1
-!                 if (job == 0) then
-!                    !                do  iq = 1, numq
-!                    qhh(ik1,ik2,ilm1,ilm2,isp) = &
-!                         qhh(ik1,ik2,ilm1,ilm2,isp) + &
-!                         dconjg(evec(i1))*evec(i2)*ewgt !(iq)
-!                    !                enddo
-!                 elseif (job == 1 .AND. ilm1 == ilm2) then
-!                    !                do  iq = 1, numq
-!                    qhh(ik1,ik2,ilm1,ilm2,isp) = &
-!                         qhh(ik1,ik2,ilm1,ilm2,isp) + &
-!                         evl*dconjg(evec(i1))*evec(i2)*ewgt !(iq)
-!                    !                enddo
-!                 endif
-!              enddo
-!           enddo
-!        enddo
-!     enddo
-!     call tcx('prlcb2')
-!   end subroutine prlcb2
-!   subroutine prlcb3(job,kmax,nlma,isp,cPkL,ewgt,evl,qpp)
-!     !- Add to local density coefficients for one state
-!     ! ----------------------------------------------------------------------
-!     !i Inputs
-!     !i   job   :0 accumulate local density-matrix
-!     !i         :1 accumulate local density-matrix weighted by energy
-!     !i   kmax  :polynomial cutoff in PkL expansion
-!     !i   nlma  :L cutoff in PkL expansion
-!     !i   isp   :spin channel
-!     !i   cPkL  :coefficients to PkL expansion of evec
-!     !i   numq  :number of trial fermi levels
-!     !i   ewgt  :eigenvector weights
-!     !i   evl   :energy weight (job=1)
-!     !o Outputs
-!     !o   qpp   :local density matrix for PkL expansion (job=0)
-!     !o         :energy-weighted local density matrix (job=1)
-!     !r Remarks
-!     !u Updates
-!     !u   05 Jul 08 (T. Kotani)
-!     !u             Option to accumulate energy-weighted output density
-!     ! ----------------------------------------------------------------------
-!     implicit none
-!     integer :: job,kmax,nlma,isp!,numq
-!     double complex cPkL(0:kmax,nlma)
-!     double precision :: qpp(0:kmax,0:kmax,nlma,nlma,isp),ewgt !(numq)
-!     real(8),optional::evl
-!     double precision :: fac
-!     integer :: iq,ilm2,ilm1,k1,k2
-!     call tcn('prlcb3')
-!     !      do  iq = 1, numq
-!     fac = ewgt !(iq)
-!     if (job == 1) fac = evl*ewgt !(iq)
-!     do  ilm2 = 1, nlma
-!        do  ilm1 = 1, nlma
-!           do  k1 = 0, kmax
-!              do  k2 = 0, kmax
-!                 qpp(k1,k2,ilm1,ilm2,isp)= qpp(k1,k2,ilm1,ilm2,isp) &
-!                      + fac*dconjg(cPkL(k1,ilm1))*cPkL(k2,ilm2)
-!              enddo
-!           enddo
-!        enddo
-!     enddo
-!     call tcx('prlcb3')
-!   end subroutine prlcb3
+  !i   ia    :site of augmentation
+  !i   nkaph :dimensions qhh,qhp
+  !i   nlmha :dimensions qhh,qhp
+  !i   kmax  :polynomial cutoff
+  !i   nlma  :augmentation L-cutoff
+  !i   isp   :spin channel
+  !i   cPkL  :PkL expansion eigenvector at site ia.
+  !i   nlmto :dimension of lmto component of basis
+  !i   evec  :eigenvector
+  !i   ewgt  :eigenvector weight
+  !i   evl   :energy weight (job=1)
+  !o Outputs
+  !o   qhh   :one-center density-matrix for PkL expansion
+  !o   eqhh :energy-weighted matrix 
+  !o   qhp   :two-center density-matrix for PkL expansion 
+  !o   eqhh  :energy-weighted matrix
+  !o   qpp   :local density matrix for PkL expansion 
+  !o  eqpp   :energy-weighted local density matrix 
   subroutine rlocbl(lfrce,nbas,isp, q,ndham,ndimh,nspc,napw,igvapw, nevec &
        ,evec,ewgt,evl,sv_p_osig,sv_p_otau,sv_p_oppi,lekkl,sv_p_oqkkl,sv_p_oeqkkl,f )
     use m_struc_def,only: s_spec,s_rv1,s_cv1,s_rv5
@@ -537,7 +415,6 @@ contains
     do  ib = 1, nbas
        if (ib == ia) cycle
        call orblib(ib)!norb,ltab,ktab,offl
-!       call gtbsl4(ib) !1(4,norb,ltab,ktab,xx,xx,ntab,blks)
        !   ... Grad of psi expansion coeffs from a virtual shift at site ib
        da=0d0 !da can be written as {d cPkL}/{d R}.See rlocb1 to make cPkL. b is used instead of db
        do io = 1, norb
@@ -566,96 +443,96 @@ contains
     call tcx('flocbl')
   end subroutine flocbl
 
-  subroutine flocb2(ia,nlmto,kmax,nkaph,nlmha,nlma,evl,evec, &
-       ppippz,sigpp,ppihpz,sighp,cPkL,wk)!lcplxp,
-    use m_orbl,only: Orblib, norb,ltab,ktab,offl, ntab,blks
-    !- Make (ppi-evl*sig)*evec
-    ! ----------------------------------------------------------------------
-    !i Inputs
-    !i   ia    :site of augmentation
-    !l   nlmto :dimension of LMTO part of hamiltonian, for hh and ht blocks
-    !i   nlmha :dimensions ppihp
-    !i   nlma  :augmentation L-cutoff
-    !i   kmax  :polynomial cutoff
-    !i   evl   :eigenvalue
-    !i   evec  :eigenvector
-    !i   nkaph :leading dimension of sigph,ppihp
-    !i   ppipp :local tail-tail potential matrix
-    !i   ppippz:local tail-tail potential matrix, complex form
-    !i         :NB: only ppipp or ppippz is used, depending on lcplxp
-    !i   sigpp :local tail-tail overlap matrix
-    !i   ppihp :local head-tail potential matrix
-    !i   ppihpz:local head-tail potential matrix, complex form
-    !i         :NB: only ppihp or ppihpz is used, depending on lcplxp
-    !i   sighp :local head-tail overlap matrix
-    !i   lcplxp=1 only now: ppi is complex
-    !i   cPkL  :PkL expansion eigenvector at site ia.
-    !o Outputs
-    !o   wk    :(ppi-evl*sig)*evec
-    !r Remarks
-    !u Updates
-    ! ----------------------------------------------------------------------
-    !     implicit none
-    ! ... Passed parameters
-    integer :: ia,kmax,nkaph,nlma,nlmha,nlmto
-    double precision :: &
-         evl,sighp(nkaph,0:kmax,0:*),& ! & ppihp(nkaph,0:kmax,nlmha,nlma),
-         sigpp(0:kmax,0:kmax,0:*)!,ppipp(0:kmax,0:kmax,nlma,nlma)
-    double complex wk(0:kmax,nlma),evec(1),cPkL(0:kmax,nlma)
-    double complex ppihpz(nkaph,0:kmax,nlmha,nlma)
-    double complex ppippz(0:kmax,0:kmax,nlma,nlma)
-    ! ... Local parameters
-    integer :: i,ilm,ilma,ilmb,io,jlm,k,k1,k2,l,ll,nlm1,nlm2,n0,nkap0,ik
-    !     .norb
-    parameter (n0=10,nkap0=3)
-!    integer :: blks(n0*nkap0),ntab(n0*nkap0)!ltab(n0*nkap0),ktab(n0*nkap0),offl(n0*nkap0),
-    double precision :: xx
-    wk=0d0
-    ! ... Add Hsm*Pkl block of ppi-evl*sig times evec
-    call orblib(ia)!norb,ltab,ktab,offl
-    !     Block evl*ppi contribution in groups of consecutive l
-!    call gtbsl4(ia) !1(4,norb,ltab,ktab,xx,xx,ntab,blks)
-    do  io = 1, norb
-       !       l,ik = l and kaph indices, needed for sigma
-       l  = ltab(io)
-       ik = ktab(io)
-       nlm1 = l**2+1
-       nlm2 = (l+1)**2
-       i  = offl(io)
-       !   ... evl*sig contribution requires explicit knowledge of l
-       do  ilmb = nlm1, nlm2
-          i = i+1
-          do  k = 0, kmax
-             wk(k,ilmb) = wk(k,ilmb) - evl*sighp(ik,k,l)*evec(i)
-          enddo
-       enddo
-       !   ... ppi contribution: loop over largest blocks possible
-       if (blks(io) /= 0) then ! .AND. lcplxp == 1) then
-          nlm2 = nlm1 + blks(io)-1
-          i  = offl(io)
-          do  ilmb = nlm1, nlm2
-             i = i+1
-             do  ilma = 1, nlma
-                do  k = 0, kmax
-                   wk(k,ilma) = wk(k,ilma) + ppihpz(ik,k,ilmb,ilma)*evec(i)
-                enddo
-             enddo
-          enddo
-       endif
-    enddo
-    ! ... Add Pkl*Pkl block of ppi-evl*sig times cPkL
-    do  ilm = 1, nlma
-       l = ll(ilm)
-       do  k1 = 0, kmax
-          do  jlm = 1, nlma
-             do  k2 = 0, kmax
-                wk(k1,ilm) = wk(k1,ilm)+ppippz(k1,k2,ilm,jlm)*cPkL(k2,jlm)
-             enddo
-          enddo
-          do  k2 = 0, kmax
-             wk(k1,ilm) = wk(k1,ilm) - evl*sigpp(k1,k2,l)*cPkL(k2,ilm)
-          enddo
-       enddo
-    enddo
-  end subroutine flocb2
+!   subroutine flocb2(ia,nlmto,kmax,nkaph,nlmha,nlma,evl,evec, &
+!        ppippz,sigpp,ppihpz,sighp,cPkL,wk)!lcplxp,
+!     use m_orbl,only: Orblib, norb,ltab,ktab,offl, ntab,blks
+!     !- Make (ppi-evl*sig)*evec
+!     ! ----------------------------------------------------------------------
+!     !i Inputs
+!     !i   ia    :site of augmentation
+!     !l   nlmto :dimension of LMTO part of hamiltonian, for hh and ht blocks
+!     !i   nlmha :dimensions ppihp
+!     !i   nlma  :augmentation L-cutoff
+!     !i   kmax  :polynomial cutoff
+!     !i   evl   :eigenvalue
+!     !i   evec  :eigenvector
+!     !i   nkaph :leading dimension of sigph,ppihp
+!     !i   ppipp :local tail-tail potential matrix
+!     !i   ppippz:local tail-tail potential matrix, complex form
+!     !i         :NB: only ppipp or ppippz is used, depending on lcplxp
+!     !i   sigpp :local tail-tail overlap matrix
+!     !i   ppihp :local head-tail potential matrix
+!     !i   ppihpz:local head-tail potential matrix, complex form
+!     !i         :NB: only ppihp or ppihpz is used, depending on lcplxp
+!     !i   sighp :local head-tail overlap matrix
+!     !i   lcplxp=1 only now: ppi is complex
+!     !i   cPkL  :PkL expansion eigenvector at site ia.
+!     !o Outputs
+!     !o   wk    :(ppi-evl*sig)*evec
+!     !r Remarks
+!     !u Updates
+!     ! ----------------------------------------------------------------------
+!     !     implicit none
+!     ! ... Passed parameters
+!     integer :: ia,kmax,nkaph,nlma,nlmha,nlmto
+!     double precision :: &
+!          evl,sighp(nkaph,0:kmax,0:*),& ! & ppihp(nkaph,0:kmax,nlmha,nlma),
+!          sigpp(0:kmax,0:kmax,0:*)!,ppipp(0:kmax,0:kmax,nlma,nlma)
+!     double complex wk(0:kmax,nlma),evec(1),cPkL(0:kmax,nlma)
+!     double complex ppihpz(nkaph,0:kmax,nlmha,nlma)
+!     double complex ppippz(0:kmax,0:kmax,nlma,nlma)
+!     ! ... Local parameters
+!     integer :: i,ilm,ilma,ilmb,io,jlm,k,k1,k2,l,ll,nlm1,nlm2,n0,nkap0,ik
+!     !     .norb
+!     parameter (n0=10,nkap0=3)
+! !    integer :: blks(n0*nkap0),ntab(n0*nkap0)!ltab(n0*nkap0),ktab(n0*nkap0),offl(n0*nkap0),
+!     double precision :: xx
+!     wk=0d0
+!     ! ... Add Hsm*Pkl block of ppi-evl*sig times evec
+!     call orblib(ia)!norb,ltab,ktab,offl
+!     !     Block evl*ppi contribution in groups of consecutive l
+! !    call gtbsl4(ia) !1(4,norb,ltab,ktab,xx,xx,ntab,blks)
+!     do  io = 1, norb
+!        !       l,ik = l and kaph indices, needed for sigma
+!        l  = ltab(io)
+!        ik = ktab(io)
+!        nlm1 = l**2+1
+!        nlm2 = (l+1)**2
+!        i  = offl(io)
+!        !   ... evl*sig contribution requires explicit knowledge of l
+!        do  ilmb = nlm1, nlm2
+!           i = i+1
+!           do  k = 0, kmax
+!              wk(k,ilmb) = wk(k,ilmb) - evl*sighp(ik,k,l)*evec(i)
+!           enddo
+!        enddo
+!        !   ... ppi contribution: loop over largest blocks possible
+!        if (blks(io) /= 0) then ! .AND. lcplxp == 1) then
+!           nlm2 = nlm1 + blks(io)-1
+!           i  = offl(io)
+!           do  ilmb = nlm1, nlm2
+!              i = i+1
+!              do  ilma = 1, nlma
+!                 do  k = 0, kmax
+!                    wk(k,ilma) = wk(k,ilma) + ppihpz(ik,k,ilmb,ilma)*evec(i)
+!                 enddo
+!              enddo
+!           enddo
+!        endif
+!     enddo
+!     ! ... Add Pkl*Pkl block of ppi-evl*sig times cPkL
+!     do  ilm = 1, nlma
+!        l = ll(ilm)
+!        do  k1 = 0, kmax
+!           do  jlm = 1, nlma
+!              do  k2 = 0, kmax
+!                 wk(k1,ilm) = wk(k1,ilm)+ppippz(k1,k2,ilm,jlm)*cPkL(k2,jlm)
+!              enddo
+!           enddo
+!           do  k2 = 0, kmax
+!              wk(k1,ilm) = wk(k1,ilm) - evl*sigpp(k1,k2,l)*cPkL(k2,ilm)
+!           enddo
+!        enddo
+!     enddo
+!   end subroutine flocb2
 end module m_rlocbl
