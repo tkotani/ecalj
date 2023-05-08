f5e304790bc7cc24bd9756d9e45ae6696cc2907b
Takao Kotani <takaokotani@gmail.com>
2023-05-08 12:16:52 +0900

    make ok! epsPP0 for q=0 is given (not documented well yet...)

--- SRC/exec/readeps.py
+++ SRC/exec/readeps.py
@@ -7,38 +7,97 @@ This is needed for error cancellation.
 '''
 import sys,os,re,glob
 import numpy as np
-
 def cleanl(iline):
     iout= [float(re.sub('D','e',x)) for x in re.split('\s+',iline) if(len(x)>0)]
     return iout
-
 def fd0(x,f,dig):
     c=np.polyfit(x[0:dig+1],f[0:dig+1],dig)
     return c[dig-1]
-    
+### interband part ###############################    
 files=glob.glob('EPS*.nlfc.dat.interbandonly')
 files.sort()
 epsfile=[open(fff,'rt').read().split('\n') for fff in files]
 dlines=['']*len(epsfile)
 q2=['']*len(epsfile)
-
 nfile=len(epsfile)
-for idf,iff in enumerate(range(nfile)):
-    dlines[idf]=[cleanl(iline) for iline in epsfile[iff][1:] if(len(cleanl(iline)))>0]
-for idf in range(nfile):   # idf is file ID for EPS*
+for idf,iff in enumerate(range(nfile)): # idf is file ID for EPS*
+    dlines[idf]=[cleanl(iline) for iline in epsfile[iff][1:] if(len(cleanl(iline)))>0] #data line for the file idf
     q= dlines[idf][0][0:4] # q vector
     q2[idf]=np.dot(q,q)    # q**2
-    print('# ',idf,q2[idf],dlines[idf][0][0:4],files[idf])
+    print('# ',idf,dlines[idf][0][0:4],files[idf])
+dinter=[]
 for idat in range(len(dlines[0])):
-    idf=1
-    omega=  dlines[0][idat][3]     #eps2  = np.array([        dlines[idf][idat][5] for idf in range(nfile)])
+    omega=  dlines[0][idat][3]     #Note: bare eps2= np.array([ dlines[idf][idat][5] for idf in range(nfile)])
     q2eps1= [q2[idf]*dlines[idf][idat][4] for idf in range(nfile)] #list of q**2*eps1 in EPS*
     q2eps2= [q2[idf]*dlines[idf][idat][5] for idf in range(nfile)] #list of q**2*eps2 in EPS*
-    eps1out = fd0(q2, q2eps1,2)
-    eps2out = fd0(q2, q2eps2,2)
-    print(idat,omega, \
-          eps2out,fd0(q2[1:],q2eps2[1:],2),fd0(q2[2:],q2eps2[2:],2),'  ', \
-          eps1out,fd0(q2[1:],q2eps1[1:],2),fd0(q2[2:],q2eps1[2:],2))
-    #,fd0(q2[3:],q2eps2[3:],2),fd0(q2[4:],q2eps2[4:],2),\#,fd0(q2[3:],q2eps1[3:],2),fd0(q2[4:],q2eps1[4:],2))
-#gnuplot -p -e 'set xran[0:30];plot "epsinter.dat" using ($2)*13.605:($3) w l,"" using ($2)*13.605:($4) w l,"" using ($2)*13.605:($5) w l,"" using ($2)*13.605:($6) w l,"" using ($2)*13.605:($7) w l,"" using ($2)*13.605:($8) w l'
-sys.exit()
+    eps1=[fd0(q2[idf:],q2eps1[idf:],2) for idf in range(nfile-2)]
+    eps2=[fd0(q2[idf:],q2eps2[idf:],2) for idf in range(nfile-2)]
+    dinter.append([idat,omega,*eps1,*eps2])
+if(nfile>0):
+    f=open('epsinter.dat','w')
+    for idf in range(nfile): print('# ',idf,dlines[idf][0][0:4],files[idf],file=f)
+    print('# recid omega eps1.... eps2... where num of eps is the number of EPSfiles',file=f) 
+    for idat in dinter:
+        print(*idat,file=f)
+    f.close()
+    f=open('epsinter.glt','w') 
+    print()
+    fdata=''
+    for iddat in range(3,nfile+3-2):
+        fdata=fdata+' '+'"epsinter.dat" using ($2)*13.605:($'+str(iddat)+') w l title "RealPart:'+ files[iddat-3]+'"'
+        fdata=fdata+',\\\n'
+    for iddat in range(3,nfile+3-2):
+        fdata=fdata+' '+'"epsinter.dat" using ($2)*13.605:($'+str(iddat+nfile-2)+') w l title   "ImagPart:'+ files[iddat-3]+'",\\\n'
+    aaa='set title "IntraBand part of Epsilon(omega(eV))"\nset xlabel "(eV)"\nset datafile fortran\nset xran[0:30]\nset yran[-30:30]\nplot\\\n' + fdata
+    print(aaa,file=f)
+    f.close()
+### intraband part ###############################    
+files=glob.glob('EPS*.nlfc.dat.intrabandonly')
+files.sort()
+epsfile=[open(fff,'rt').read().split('\n') for fff in files]
+dlines=['']*len(epsfile)
+q2=['']*len(epsfile)
+nfile=len(epsfile)
+for idf,iff in enumerate(range(nfile)): # idf is file ID for EPS*
+    dlines[idf]=[cleanl(iline) for iline in epsfile[iff][1:] if(len(cleanl(iline)))>0] #data line for the file idf
+    print('# ',idf,dlines[idf][0][0:3],files[idf])
+dintra=[]
+for idat in range(len(dlines[0])):
+    omega= dlines[0][idat][3]     
+    eps1= [dlines[idf][idat][4] for idf in range(nfile)] 
+    eps2= [dlines[idf][idat][5] for idf in range(nfile)] 
+    dintra.append([idat,omega,*eps1,*eps2])
+f=open('epsintra.dat','w')
+for idf in range(nfile): print('# ',idf,dlines[idf][0][0:4],files[idf],file=f)
+print('# recid omega eps1.... eps2... where num of eps is the number of EPSfiles',file=f) 
+for idat in dintra:
+    print(*idat,file=f)
+f.close()    
+print()
+f=open('epsintra.glt','w')
+fdata=''
+for iddat in range(3,nfile+3):
+    fdata=fdata+' '+'"epsintra.dat" using ($2)*13.605:($'+str(iddat)+') w l title "RealPart:'+ files[iddat-3]+'",\\\n'
+for iddat in range(3,nfile+3):
+    fdata=fdata+' '+'"epsintra.dat" using ($2)*13.605:($'+str(iddat+nfile)+') w l title  "ImagPart:'+ files[iddat-3]+'",\\\n'
+aaa='set title "IntraBand part of Epsilon(omega(eV))"\nset xlabel "(eV)"\nset datafile fortran\nset xran[0:30]\nset yran[-30:30]\n'+\
+    'plot\\\n' + fdata
+print(aaa,file=f)
+f.close()
+#### epsall.data = epsinter+epsintra. Only with 1st data ####    
+f=open('epsall.dat','w')
+for idat in range(len(dintra)):
+    eps1= dinter[idat][2]
+    eps2= dinter[idat][2+nfile-2]
+    id,omega=dintra[idat][0:2]
+    if(len(dintra)!=0):
+        eps1+=dintra[idat][2]
+        eps2+=dintra[idat][2+nfile]
+    print(id,omega, eps1, eps2,file=f)
+f.close()    
+f=open('epsall.glt','w')
+aaa='set title "Epsilon(omega(eV))"\nset xlabel "(eV)"\nset datafile fortran\nset xran[0:30]\nset yran[-30:30]\n'+\
+    'plot\\\n' + '"epsall.dat" using ($2)*13.605:($3) w l title "RealPart","" using ($2*13.605):($4) w l title "ImagPart"'
+print(aaa,file=f)
+f.close()
+print('OK! We have generated epsinter.dat,epsinter.glt, epsintra.*(if metal) epsall.*')
