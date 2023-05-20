519fc68e78dcaa2962de4e3e459224dbc6bbc93d
Takao Kotani <takaokotani@gmail.com>
2023-05-20 10:56:03 +0900

    saito report: llnew is needed for Co5Gd LMXA=6.

--- SRC/subroutines/llnew.f90
+++ SRC/subroutines/llnew.f90
@@ -1,10 +1,9 @@
 module m_ll
   contains
   pure integer function ll(ilm) ! Returns l, given lm index
-    intent(in):: ilm
-    integer :: m,l,ilm
-    integer,parameter:: lmaxx=16
-    integer,parameter :: lla(1:(lmaxx+1)**2) = [((l,m=1,2*l+1),l=0,16)]
+    intent(in)::           ilm
+    integer ::  ilm,m,l
+    integer,parameter:: lmaxx=24, lla(1:(lmaxx+1)**2) = [((l,m=1,2*l+1),l=0,lmaxx)]
     ll = lla(ilm)
   end function ll
 endmodule
