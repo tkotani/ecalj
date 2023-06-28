768f684d2aab6e66d6640372c90cdca008fe036e
takao kotani <takaokotani@gmail.comm>
2023-06-28 19:24:46 +0900

    Add ecalj/MATERIALS/IIR/README.org for minimum explanation to perform Impact ionization rate calculation

---
+++ MATERIALS/IIR/C/save.c
@@ -0,0 +1,62 @@
+Start LMF c
+Start LMFA c
+Start LMF c
+h ehf(eV)=-2058.678714 ehk(eV)=-2057.710922 sev(eV)= 2.789869
+i ehf(eV)=-2058.337593 ehk(eV)=-2057.817681 sev(eV)= 2.627860
+i ehf(eV)=-2057.989149 ehk(eV)=-2057.915516 sev(eV)= 1.829037
+i ehf(eV)=-2057.964248 ehk(eV)=-2057.925609 sev(eV)= 1.668905
+i ehf(eV)=-2057.937659 ehk(eV)=-2057.937609 sev(eV)= 1.275972
+i ehf(eV)=-2057.937654 ehk(eV)=-2057.937623 sev(eV)= 1.278679
+c ehf(eV)=-2057.937663 ehk(eV)=-2057.937661 sev(eV)= 1.293427
+Start LMFA c
+Start LMFGWD --jobgw=0 c
+Start LMF c
+i ehf(eV)=-2057.937662 ehk(eV)=-2057.937660 sev(eV)= 1.292655
+c ehf(eV)=-2057.937660 ehk(eV)=-2057.937659 sev(eV)= 1.292307
+Start LMFGWD c --jobgw=0
+Start LMFGWD c --jobgw=1
+Start LMF c
+i ehf(eV)=-2063.716203 ehk(eV)=-2063.716175 sev(eV)=-4.486401
+i ehf(eV)=-2063.716208 ehk(eV)=-2063.716191 sev(eV)=-4.490133
+c ehf(eV)=-2063.716260 ehk(eV)=-2063.716258 sev(eV)=-4.504965
+Start LMF c
+i ehf(eV)=-2057.937651 ehk(eV)=-2057.937619 sev(eV)= 1.272877
+i ehf(eV)=-2057.937651 ehk(eV)=-2057.937620 sev(eV)= 1.272656
+i ehf(eV)=-2057.937651 ehk(eV)=-2057.937632 sev(eV)= 1.275745
+c ehf(eV)=-2057.937671 ehk(eV)=-2057.937669 sev(eV)= 1.289622
+Start LMFGWD c --jobgw=0
+Start LMFGWD c --jobgw=1
+Start LMF c
+i ehf(eV)=-2063.716737 ehk(eV)=-2063.716704 sev(eV)=-4.488846
+i ehf(eV)=-2063.716749 ehk(eV)=-2063.716730 sev(eV)=-4.492020
+c ehf(eV)=-2063.716818 ehk(eV)=-2063.716816 sev(eV)=-4.505662
+Start LMFGWD c --jobgw=0
+Start LMFGWD c --jobgw=1
+Start LMF c
+i ehf(eV)=-2063.714794 ehk(eV)=-2063.714792 sev(eV)=-4.504354
+c ehf(eV)=-2063.714788 ehk(eV)=-2063.714786 sev(eV)=-4.504855
+Start LMFGWD c --jobgw=0
+Start LMFGWD c --jobgw=1
+Start LMF c
+i ehf(eV)=-2064.965896 ehk(eV)=-2064.965878 sev(eV)=-5.757026
+i ehf(eV)=-2064.965917 ehk(eV)=-2064.965907 sev(eV)=-5.756752
+c ehf(eV)=-2064.965999 ehk(eV)=-2064.965997 sev(eV)=-5.756413
+Start LMFGWD c --jobgw=0
+Start LMFGWD c --jobgw=1
+Start LMF c
+i ehf(eV)=-2065.067833 ehk(eV)=-2065.067831 sev(eV)=-5.858891
+c ehf(eV)=-2065.067826 ehk(eV)=-2065.067825 sev(eV)=-5.859657
+Start LMFGWD c --jobgw=0
+Start LMFGWD c --jobgw=1
+Start LMF c
+i ehf(eV)=-2065.078668 ehk(eV)=-2065.078666 sev(eV)=-5.873046
+c ehf(eV)=-2065.078669 ehk(eV)=-2065.078668 sev(eV)=-5.872948
+Start LMFA c
+Start LMFGWD --jobgw=0 c
+Start LMF c --quit=band
+i ehf(eV)=-2065.078674 ehk(eV)=-2065.078672 sev(eV)=-5.872657
+Start LMF c --band
+Start LMCHK c
+Start LMF c --quit=band
+i ehf(eV)=-2065.078674 ehk(eV)=-2065.078672 sev(eV)=-5.872657
+Start LMF c --band
