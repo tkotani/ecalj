0062e44445dfd313097fa136891cb56bfeccfb28
Takao Kotani <takaokotani@gmail.com>
2023-12-14 17:01:14 +0900

    make 0K! addinv bugfix by obata

--- SRC/subroutines/m_lmfinit.f90
+++ SRC/subroutines/m_lmfinit.f90
@@ -554,7 +554,8 @@ contains
       if( (lrlxr>=1.AND.lrlxr<=3) .OR. cmdopt0('--cls') .OR. cmdopt0('--nosym') .OR. cmdopt0('--pdos')) then
          symg = 'e'
          addinv = .false. 
-      elseif( lso==0 .and. sum(abs(idu))/=0 .and. (.not.sexist) ) then ! Add inversion means Hamiltonian is real (time-reversal).
+!     elseif( lso==0 .and. sum(abs(idu))/=0 .and. (.not.sexist) ) then ! Add inversion means Hamiltonian is real (time-reversal).
+      elseif( lso==0 .and. sum(abs(idu))==0 .and. (.not.sexist) ) then ! Add inversion means Hamiltonian is real (time-reversal). !bugfix by obata 2023-12-12
          addinv=.true. 
       else
          addinv=.false. 
