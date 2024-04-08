b94451b5c7878050b4743bee8a16390869f9bee9
Takao Kotani <takaokotani@gmail.com>
2024-04-08 11:18:15 +0900

    lmfham1 lmfham2 working

--- SRC/main/lmfham1.f90
+++ SRC/main/lmfham1.f90
@@ -1,3 +1,12 @@
 program main
-   call lmfham1()
-endprogram
+  use m_cmdpath,only: setcmdpath
+  use m_args,only: m_setargs
+  use m_lmfham1,only: lmfham1
+  integer:: ierr
+  call mpi_init(ierr)
+  call setcmdpath()
+  call m_setargs()
+  call lmfham1()
+  call mpi_finalize(ierr)
+  call exit(0)
+end program main
--- SRC/main/lmfham2.f90
+++ SRC/main/lmfham2.f90
@@ -1,3 +1,12 @@
 program main
-   call lmfham2()
-endprogram
+  use m_cmdpath,only: setcmdpath
+  use m_args,only: m_setargs
+  use m_lmfham2,only: lmfham2
+  integer:: ierr
+  call mpi_init(ierr)
+  call setcmdpath()
+  call m_setargs()
+  call lmfham2()
+  call mpi_finalize(ierr)
+  call exit(0)
+end program main
