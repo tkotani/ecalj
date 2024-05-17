98ba3e1949bd263454b919a2484e07294c830624
Takao Kotani <takaokotani@gmail.com>
2024-05-17 22:32:01 +0900

    corrected testecalj.py

--- SRC/TestInstall/testecalj.py
+++ SRC/TestInstall/testecalj.py
@@ -52,7 +52,7 @@ if(showt):
 "Install to bin"
 os.makedirs(bindir,exist_ok=True)
 exec='lmfa lmf run_arg job_pdos job_tdos ctrl2ctrlp.py a2vec.py \
- gwutil.py gwsc qg4gw hvccfp0 hsfp0_sc hqpe_sc hmaxloc hpsig_MPI huumat_MPI hwmatK_MPI hx0init hrcxq \
+ gwutil.py gwsc qg4gw hvccfp0 hsfp0_sc hqpe_sc hmaxloc hpsig_MPI huumat_MPI hwmatK_MPI hrcxq \
  rdata4gw_v2 heftet hbasfp0 gw_lmfh hx0fp0 hsfp0 hqpe eps_lmfh epsPP_lmfh epsPP_lmfh_chipm genMLWFx'
 for ex in exec.split():
     shutil.copy(ecaljroot+'/SRC/exec/'+ex,bindir)
