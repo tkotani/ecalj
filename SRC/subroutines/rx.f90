8cd094dcb7f70437cc69e160de68d770179b89bd
Takao Kotani <takaokotani@gmail.com>
2024-07-11 16:25:15 +0900

    make ok! move finits.f90 to rx.f90 cleaning

--- SRC/subroutines/rx.f90
+++ SRC/subroutines/rx.f90
@@ -1,69 +1,74 @@
 ! All exit routines with errors 2023 feb. Too much varieties is historical reasons.
 subroutine rx(string) !error exit
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_lgunit,only:stdo,stdl
   use m_mpi,only:   comm
   integer::ierr
   character*(*) string
-  write(stdo,*)trim(string)
+  write(error_unit,*) trim(string)
   call MPI_Abort(COMM, 9110000)
-!  call fexit0(-1,string)
-!  call rx0s(string)         !for single core exit
 end subroutine rx
 subroutine rxi(string,iarg) ! Error exit, with a single integer at end
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_lgunit,only:stdo,stdl
   use m_mpi,only:   comm
   character*(*) string
   integer:: iarg
   character(10):: i2char
-  write(stdo,*)trim(' Exit -1 '//string//' '//trim(i2char(iarg)))
+  write(error_unit,*)trim(' Exit -1 '//string//' '//trim(i2char(iarg)))
   call MPI_Abort(COMM, 9110010)
 end subroutine rxi
 subroutine rxii(string,iarg,iarg2) 
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_lgunit,only:stdo,stdl
   use m_mpi,only:   comm
   character*(*) string
   integer:: iarg,iarg2
   character(10):: i2char
-  write(stdo,*)trim(' Exit -1 '//string//' '//trim(i2char(iarg))//' '//trim(i2char(iarg2)))
+  write(error_unit,*)trim(' Exit -1 '//string//' '//trim(i2char(iarg))//' '//trim(i2char(iarg2)))
   call MPI_Abort(COMM, 9110011)
 end subroutine rxii
 subroutine rxiii(string,iarg,iarg2,iarg3) 
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_lgunit,only:stdo,stdl
   use m_mpi,only:   comm
   character*(*) string
   integer:: iarg,iarg2,iarg3
   character(10):: i2char
-  write(stdo,*)trim(' Exit -1 '//string//' '//trim(i2char(iarg))//' '//trim(i2char(iarg2))//' '//trim(i2char(iarg3)))
+  write(error_unit,*)trim(' Exit -1 '//string//' '//trim(i2char(iarg))//' '//trim(i2char(iarg2))//' '//trim(i2char(iarg3)))
   call MPI_Abort(COMM, 9110111)
 end subroutine rxiii
 subroutine rx1(string,arg) ! Error exit, with a single argument
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_ftox
   use m_lgunit,only:stdo,stdl
   use m_mpi,only:   comm
   character(15):: f2a
   character*(*) string
   double precision :: arg
-  write(stdo,*)trim(' Exit -1 '//string//trim(ftof(arg)))
+  write(error_unit,*)trim(' Exit -1 '//string//trim(ftof(arg)))
   call MPI_Abort(COMM, 9120001)
 end subroutine rx1
 subroutine rx2(string,arg1,arg2) ! Error exit, with two arguments
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_lgunit,only:stdo,stdl
   use m_ftox
   use m_mpi,only:   comm
   character(15):: f2a
   character*(*) string
   double precision :: arg1,arg2
-  write(stdo,*)trim(' Exit -1 '//string//trim(ftof(arg1))//' '//trim(ftof(arg2)))
+  write(error_unit,*)trim(' Exit -1 '//string//trim(ftof(arg1))//' '//trim(ftof(arg2)))
   call MPI_Abort(COMM, 9120011)
 end subroutine rx2
 subroutine rx3(string,arg1,arg2,arg3) ! Error exit, with two arguments
+  use, intrinsic :: iso_fortran_env, only: error_unit
   use m_lgunit,only:stdo,stdl
   use m_ftox
   use m_mpi,only:   comm
   character(15):: f2a
   character*(*) string
   real(8):: arg1,arg2,arg3
-  write(stdo,*)trim(' Exit -1 '//string//trim(ftof(arg1))//' '//trim(ftof(arg2))//' '//trim(ftof(arg3)))
+  write(error_unit,*)trim(' Exit -1 '//string//trim(ftof(arg1))//' '//trim(ftof(arg2))//' '//trim(ftof(arg3)))
   call MPI_Abort(COMM, 91200111)
 end subroutine rx3
 subroutine rxs(string,msg) ! Error exit with extra string message
@@ -78,26 +83,19 @@ subroutine rxx(test,string)
   character*(*) string
   if (test) call rx(string)
 end subroutine rxx
-
-
+!----------- Normal exit
 subroutine rx0s(string) !normal exit for master_mpi
   use m_MPItk,only: master_mpi
   character*(*) string
   if(master_mpi) write(6,"(/,a)") trim(string)//' ======================'
   call exit(0)
 end subroutine rx0s
-subroutine rx0(string) !normal exit
-  use m_lgunit,only:stdo,stdl
-  character*(*) string
-  call fexit0(0,string)
-  call rx0s(string)         !for single core exit
-end subroutine rx0
-subroutine fexit0(retval,strng)! retval:  return value passed to operating system. /=0 for error exit
+subroutine rx0(strng)! Normal exit
   use m_mpi,only:   comm1=>comm
   use m_MPItk,only: comm2=>comm,readtk
   use m_lgunit,only:stdo,stdl
   implicit none
-  integer :: retval,iopt,abret
+  integer :: iopt,abret
   character*(*) strng
   double precision :: args,arg2,arg3,argss(3)
   integer :: iprint,i,i2,scrwid,ix
@@ -131,12 +129,7 @@ subroutine fexit0(retval,strng)! retval:  return value passed to operating syste
      endif
      call tcprt(stdo)
   endif
-  if(retval/=0) then
-     write(stdo,"(a,i0,a,i0,a,3d15.8)")"ERROR Exit ",retval,' procid= ',procid,' '//trim(strng)
-     call MPI_Abort(COMM, 911)
-  else
-     if(procid==master) write(stdo,"(a,i0,a,3d15.8)") 'Exit 0 procid= ',procid,' '//trim(strng)
-  endif
+  if(procid==master) write(stdo,"(a,i0,a,3d15.8)") 'Exit 0 procid= ',procid,' '//trim(strng)
   call MPI_FINALIZE(ierr)
-  call exit(retval)
-end subroutine fexit0
+  call exit(0)
+end subroutine rx0
