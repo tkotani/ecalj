72d89b9e76cc8e570eb589a5a488e0edcd4dbe61
takao kotani <takaokotani@gmail.comm>
2025-04-10 18:57:17 +0900

    ecalj_auto jobtest1.sh jobtestSGCkmeshfix.sh

---
+++ ecalj_auto/jobtest1.sh
@@ -0,0 +1,5 @@
+python --version
+python ./auto/jobsubmit.py --inpath INPUT/test1 --epath ${HOME}/bin/ --nqsub 2 --ncore 16 --bnd4all True --niter 2
+#python ./auto/jobsubmit.py --inpath INPUT/testSGA --epath ${HOME}/bin/ --nqsub 2 --ncore 16 --bnd4all True --niter 2 --kkmesh 4 4 4 3 3 3 
+# python ./auto/jobsubmit.py --inpath INPUT/testSGA --epath /home/k0413/k041303/bin/ --nqsub 1 --ncore 128 --bnd4all True --niter 2
+#        auto/jobsubmit.py --inpath INPUT/testSGA --epath /home/stakano/ecalj/bin/ --nsub 2 --niter 2 --ncore 32 --bnd4all True --gw80 False --koption 6 8 10
