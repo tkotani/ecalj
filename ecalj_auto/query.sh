805e4924fa296e8ca4e916617f67e3439dc3b728
Takao Kotani <takaokotani@gmail.com>
2025-04-25 14:19:28 +0900

    add gw1500 POSCAR

--- ecalj_auto/query.sh
+++ ecalj_auto/query.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+# I use pymatgen 2024.10.3 with Python 3.13.3
+#       pymatgen 2023.5.10 with Python 3.9.13
+
 time=$(date +"%Y%m%d-%H%M%S")
 dir=$time
 ### for test (Si Ga Al)
-./auto/mpquery.py --nsites 1 2 --include Si Ga Al --dir=testSGAXXX
+./auto/mpquery.py --nsites 1 2 --include Si Ga Al --dir=testSGAtest
+
+exit
+
 #./auto/mpquery.py --nsites 1 2 --include Si Ga Al --dir=$dir/SimpleTest
 #./auto/mpquery.py --nsites 2 3 --fname joblist_nsites3 --dir=testSGA_nsite23
 
 ### gw1000 ###
 #./auto/mpquery.py --dir=$dir/default
 ./auto/mpquery.py --dir=gw1000
+./auto/mpquery.py --dir=gw1500 --nsites 1 8 
+
 ### a sample including Tl only but with the gw1000 condition. No LN AC, only expt
 #./auto/mpquery.py --include Tl --dir testTl
 
