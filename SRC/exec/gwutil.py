0dbf45d91f2cf4a217fdca596c5816c8ad4b8bc9
Masao Obata <obata.mso@gmail.com>
2025-07-06 22:57:49 +0900

    merge of run_program for automatic change of # of para

--- SRC/exec/gwutil.py
+++ SRC/exec/gwutil.py
@@ -18,23 +18,79 @@ def remove(fname):
 def rm_files(files):
     for fname in files:
         remove(fname)
+def run_program(commandline, ncore=0,x0=0):
+    import subprocess,datetime
+    xdate=datetime.datetime.now() #today().isoformat()
+    mpirun='mpirun --bind-to none --map-by node -np %d '%ncore if ncore!=0 else ''
+    run_command = mpirun + commandline
+    if(x0==0):
+        print(xdate,'  '+run_command,flush=True)
+    else:
+        print(xdate-x0,'  '+run_command,flush=True)
+    info=subprocess.run(run_command,shell=True)
+    if info.returncode!=0: #if return error
+        print('Error in '+run_command,flush=True)
+        exit(-1)
+    return xdate
 
-nxdict = {}
-def run_program(cmd, ncore=0, x0=0):
-    import subprocess, datetime
-    global nxdict
-    nx = ncore
-    while nx > 0:
-        t = datetime.datetime.now()
-        if cmd in nxdict and nx==ncore:
-            run_cmd = nxdict[cmd]
-        else:
-            run_cmd = (f'mpirun -np {nx} ' if ncore != 0 else '') + cmd
-        print(t if x0 == 0 else t - x0, ' ', run_cmd, flush=True)
-        result = subprocess.run(run_cmd, shell=True)
-        if result.returncode == 0:
-            nxdict[cmd] = run_cmd
-            return t
-        print(f'Error for ncore={nx} in {run_cmd}', flush=True)
-        nx //= 2
-    exit(-1)
+# import os, datetime, shutil, glob
+# def gw_args(pname,note):
+#     '''
+#     arguments settings
+#     Returns
+#       target: target material name ctrl.target
+#       ncore: number of MPI thereads in lmf
+#       option: options
+#     '''
+#     import argparse
+#     parser=argparse.ArgumentParser(prog=pname,description=note)
+#     parser.add_argument("-np",     help='number of mpi cores in lmf',action='store')
+#     parser.add_argument("material_name",help=':name of extension')
+#     parser.add_argument('--phispinsym',action='store_true',help='spin-symmetrized augmentation')
+# #    parser.add_argument('--afsym',action='store_true',help='AF symmetry mode')
+#     args=parser.parse_args()
+#     print(args)
+#     target=args.material_name
+#     ncore=int(args.np)
+#     option=''
+#     if args.phispinsym==True: option=' --phispinsym'
+#     return(target,ncore,option)
+
+# def gwsc_args():
+#     '''
+#     arguments settings
+#     Returns
+#       target: target material name ctrl.target
+#       nloop: numbdr of QSGW iterations starting from current result (nloop=0 is replaced by nloop=1 internally)
+#       ncore: number of MPI thereads in lmf
+#       ncore2: number of MPI thereads in lxsC etc.
+#       option: options
+#     '''
+#     import argparse
+#     parser=argparse.ArgumentParser(prog='gwsc',description='QSGW calculation')
+#     parser.add_argument("-np",     help='number of mpi cores in lmf',action='store')
+#     parser.add_argument("-np2",    help='number of mpi cores in lxc etc.',action='store') 
+#     parser.add_argument("nloop",   help='iteration number of QSGW loop')
+#     parser.add_argument("material_name",help='material name')
+#     parser.add_argument('--phispinsym',action='store_true',help='spin-symmetrized augmentation')
+# #    parser.add_argument('--emptyrun',action='store_true',help='test for gprof for memory')
+# #    parser.add_argument('--afsym',action='store_true',help='AF symmetry mode')
+#     args=parser.parse_args()
+#     print(args)
+#     target=args.material_name
+#     nloop=int(args.nloop)
+#     if args.np!=None:
+#         ncore=int(args.np)
+#         if args.np2!=None:
+#             ncore2=int(args.np2)
+#         else:
+#             ncore2=ncore
+#     else:
+#         ncore=1
+#         ncore2=ncore
+#     option=''
+#     if args.phispinsym==True: option=' --phispinsym'
+# #    if args.emptyrun==True: option=option+' --emptyrun'
+# #    if args.afsym==True: option=option+' --afsym'
+#     return(target,nloop,ncore,ncore2,option)
+
