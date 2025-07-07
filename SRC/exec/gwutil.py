a010ae225e6e13cdfe4fef15db2902d77fcadd58
Takao Kotani <takaokotani@gmail.com>
2025-07-07 12:44:17 +0900

    gwutil.py bugfix

--- SRC/exec/gwutil.py
+++ SRC/exec/gwutil.py
@@ -41,15 +41,22 @@ def run_program(cmd, ncore=0, x0=0):
 
 const_b={}
 def run_program_breduction(cmd, ncore=0, x0=0, ext=''):
+    """Run a program with b-mix reduction.
+       Decreasing b until lmf converges."""
     import subprocess, datetime,shutil,glob,re,os
     global const_b
-    bval=0.2
+    with open(f'ctrl.{ext}', 'r') as f:
+        text = f.read()
+        bval = float(re.search(r'b=0?\.\d+', text).group().split('=')[1])
+        
     while ( bval > 0.05): #bmix reducing
         t = datetime.datetime.now()
-        if( os.path.isfile(f'rst.{ext}') ):  shutil.copy(f'rst.{ext}', f'rst.{ext}.bk')
+        if os.path.isfile(f'rst.{ext}'):
+            shutil.copy(f'rst.{ext}', f'rst.{ext}.bk')
         if cmd in const_b: bval= const_b[cmd]
         run_cmd = (f'mpirun -np {ncore} ' if ncore != 0 else '') + cmd
         print(t if x0 == 0 else t - x0, ' ', run_cmd, flush=True)
+
         #result = subprocess.run(run_cmd, shell=True)
         result = subprocess.run(run_cmd, shell=True, stderr=subprocess.PIPE)
         if result.returncode != 0:
@@ -63,14 +70,16 @@ def run_program_breduction(cmd, ncore=0, x0=0, ext=''):
                 const_b[cmd] = bval
                 return t
         print(f'Error for const_b={bval} in {run_cmd}', flush=True)
+        for file in glob.glob("mix*"): os.remove(file)
+        os.remove(f'rst.{ext}')
+        shutil.copy(f'rst.{ext}.bk',f'rst.{ext}')
+
         bval = round(bval - 0.05, 2)
+        print(f'Set b={bval} in {run_cmd}', flush=True)
         with open(f'ctrl.{ext}', 'r') as f:
             text = f.read()
         text_new = re.sub(r'b=0?\.\d+', f'b={bval}', text)
         with open(f'ctrl.{ext}', 'w') as f:
             f.write(text_new)
-        for file in glob.glob("mix*"): os.remove(file)
-        os.remove(f'rst.{ext}')
-        shutil.copy(f'rst.{ext}.bk',f'rst.{ext}')
         
     exit(-1)
