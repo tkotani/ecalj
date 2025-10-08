2c6150f02f22aba8e18fba6502535592c3d476ad
Takao Kotani <takaokotani@gmail.com>
2025-10-09 07:13:43 +0900

    minor

--- Samples/Magnon/Fe_magnon/test.py
+++ Samples/Magnon/Fe_magnon/test.py
@@ -2,26 +2,31 @@ from comp import test2_check,runprogs
 def test(args,bindir,testdir,workdir): #Fixed. called as >testecalj Fe_magnon
     tall=''
     MATERIAL="fe"
-    NSLOTS=args.np
+    ncore=args.np
     lmfa= f'mpirun -np 1 {bindir}/lmfa '
     lmf = f'mpirun -np {args.np} {bindir}/lmf '
     outfile='out.lmf.fe'
-    runprogs([
+    if(args.checkonly): runprogs([
+            "rm -rf summary.txt"
+            ],quiet=True)
+    else: runprogs([
         lmfa + f" {MATERIAL} > "+ outfile,
         lmf  + f" {MATERIAL} > "+ outfile,
-        f"{bindir}/job_band   {MATERIAL} -np {NSLOTS}",              # band plot
-        f"{bindir}/genMLWF_vw {MATERIAL} -np {NSLOTS}",              # Wannier
+        f"{bindir}/job_band   {MATERIAL} -np {ncore}",              # band plot
+        f"{bindir}/genMLWF_vw {MATERIAL} -np {ncore}",              # Wannier
         "echo --- Go into epsPP_magnon. It may take several minutes ---",
         "date",
-        f"{bindir}/epsPP_magnon_chipm_mpi -np {NSLOTS} {MATERIAL}",  # magnon calculation
+        f"{bindir}/epsPP_magnon_chipm_mpi -np {ncore} {MATERIAL}",  # magnon calculation
         "date",
         "gnuplot fbplot.glt" ,
         "gnuplot wanplot.glt",
         "gnuplot mag3d.glt"
     ])
     dat='wan_ChiPMz.mat.syml1'
+    print(dat,end=': ')
     tall+=test2_check(testdir+'/'+dat, workdir+'/'+dat) #numerical agreement check
     dat='wan_ChiPMr.mat.syml1'
+    print(dat,end=': ')
     tall+=test2_check(testdir+'/'+dat, workdir+'/'+dat)
     message1='''
      ======================================================
