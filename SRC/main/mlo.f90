14cdbdf1f1ed7c9f2e7f36beedfbc54981d81e30
Takao Kotani <takaokotani@gmail.com>
2025-11-01 14:28:24 +0900

    Cmake modified. MLOsamples/README.md

---
+++ SRC/main/mlo.f90
@@ -0,0 +1,12 @@
+program main
+  use m_cmdpath,only: setcmdpath
+  use m_args,only: m_setargs
+  use m_mlo,only: mlo
+  integer:: ierr
+  call mpi_init(ierr)
+  call setcmdpath()
+  call m_setargs()
+  call mlo()
+  call mpi_finalize(ierr)
+  call exit(0)
+end program main
--- Samples/MLWF_sampls/README_wannier.md
+++
@@ -1,2 +0,0 @@
-* How to run samples?
-See UsageDetails.md in ecaljdoc.
