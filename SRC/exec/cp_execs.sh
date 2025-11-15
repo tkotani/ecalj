a4b21ee4ebe093722dbaa3faa94a2e3cf2235b0f
Masao Obata <obata.mso@gmail.com>
2025-11-15 20:07:52 +0900

    add cp_exec function in cmakelist.txt

--- SRC/exec/cp_execs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd "$(dirname "$0")/build"
-for f in *; do
-  if [ -f "$f" ] && [ -x "$f" ]; then
-    target="../$f"
-    [ -e "$target" ] && rm -f "$target"
-    cp "$f" "$target"
-  fi
-done
