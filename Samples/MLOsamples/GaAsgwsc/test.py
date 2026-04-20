53b0330afa7415c062f7ea89a78351a30d78c6c2
Takao Kotani <takaokotani@gmail.com>
2026-04-20 10:37:50 +0900

    GaAsgwsc: minimal test directory with both non-SOC and SOC tests
    Move old fully-populated GaAsgwsc -> GaAsgwsc.bk and create a minimal
    set runnable by testecalj:
    
    Kept inputs:
      ctrl.gaas, ctrls.gaas, GWinput, syml.gaas, qplist.dat,
      rst.gaas, sigm.gaas, atmpnu.{1,2}.gaas, bnd00[1-6].spin1 (plot overlay).
    
    Reference outputs:
      band_MLO_spin1.dat       (non-SOC job_mlo, 1746 lines, VBM 3-fold at Gamma)
      band_MLO_spin1.soc.dat   (SOC job_mlo_soc, 3492 lines, Delta_SO=0.336 eV)
    
    test.py runs two sub-tests:
      1. job_mlo gaas        -> compare band_MLO_spin1.dat vs reference
      2. job_mlo_soc gaas    -> compare band_MLO_spin1.dat vs band_MLO_spin1.soc.dat
    Tolerance 7.4e-5 Ry (~0.001 eV). Both PASSED.

--- Samples/MLOsamples/GaAsgwsc/test.py
+++ Samples/MLOsamples/GaAsgwsc/test.py
@@ -1,13 +1,32 @@
 from comp import test2_check, runprogs, rmfiles
+import glob, os
+
 def test(args, bindir, testdir, workdir):
-    job_mlo = f'{bindir}/job_mlo '
+    job_mlo     = f'{bindir}/job_mlo '
+    job_mlo_soc = f'{bindir}/job_mlo_soc '
     npflag = f'-np {args.np} '
-    out = 'band_MLO_spin1.dat'
     tall = ''
-    rmfiles(workdir, ['lwriteham', 'lmlo', 'HamRsMLO', out])
-    runprogs([
-        'rm -f HamiltonianPMT.* PROCAR.UP.* PROCAR.DN.* PROCAR.UP PROCAR.DN __HamiltonianPMT*',
-        f'{job_mlo} gaas {npflag} --NoGnuplot',
-    ])
-    tall += test2_check(testdir + '/' + out, workdir + '/' + out, abs_tol=7.4e-5)  # 0.001 eV in Ry
+
+    def cleanup():
+        for pat in ('HamiltonianPMT.*', '__HamiltonianPMT*', 'PROCAR.UP.*', 'PROCAR.DN.*'):
+            for p in glob.glob(os.path.join(workdir, pat)):
+                try: os.remove(p)
+                except OSError: pass
+        rmfiles(workdir, ['HamRsMLO', 'band_MLO_spin1.dat', 'band_MLO_spin2.dat',
+                          'PROCAR.UP', 'PROCAR.DN', 'lwriteham', 'lmlo', 'llmf_ef_soc',
+                          'bandplot_MLO.isp1.glt', 'bandplot_MLO.isp2.glt'])
+
+    # --- Test 1: non-SOC MLO bands ---
+    cleanup()
+    runprogs([f'{job_mlo} gaas {npflag} --NoGnuplot'])
+    tall += test2_check(os.path.join(testdir, 'band_MLO_spin1.dat'),
+                        os.path.join(workdir, 'band_MLO_spin1.dat'),
+                        abs_tol=7.4e-5)  # 0.001 eV in Ry
+
+    # --- Test 2: SOC MLO bands via job_mlo_soc ---
+    cleanup()
+    runprogs([f'{job_mlo_soc} gaas {npflag} --NoGnuplot'])
+    tall += test2_check(os.path.join(testdir, 'band_MLO_spin1.soc.dat'),
+                        os.path.join(workdir, 'band_MLO_spin1.dat'),
+                        abs_tol=7.4e-5)
     return tall
