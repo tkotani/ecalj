e4555b60ecca856ca2d435a97b058c956525d04c
Takao Kotani <takaokotani@gmail.com>
2026-06-03 12:32:10 +0900

    viewvesta / ctrl2vasp: accept the canonical ctrlg.<sname>.toml input
    The post-2026-05 input file for lmf is `ctrlg.<sname>.toml`. The two
    structure-viewing helpers still only knew about the legacy text forms
    (`ctrl.<sname>`, `ctrls.<sname>`), so e.g. `viewvesta ctrlg.nio.toml`
    silently fell into the `ctrl.` branch -> `re.sub('ctrl.','POSCAR_',
    'ctrlg.nio.toml') = 'POSCAR_g.nio.toml'` and produced garbage.
    
    ctrl2vasp.py: add a TOML branch ahead of the legacy parsing.
    
      if ctrlg.<sname>.toml: tomllib.load() -> ALAT = struc.alat *
      0.529177, PLAT_list = flat(struc.plat), atomlist = [[atom, x, y, z]
      for site in [[site]]]. Then convctrl.savefile(ALAT, PLAT_list,
      atomlist, 'POSCAR_<sname>', 'Cartesian') -- the same call shape the
      legacy code already uses, so the resulting POSCAR groups
      consecutive same-name sites into one VASP atom-type block exactly
      as before. Cartesian-only matches the existing behaviour.
    
    viewvesta.py: detect ctrlg.*.toml BEFORE the `ctrl` prefix check
    (order matters: 'ctrlg.foo.toml'[0:4]=='ctrl' would otherwise win and
    mangle the filename), compute fname = POSCAR_<sname>.vasp, and hand
    off to ctrl2vasp -- which now handles all three input forms.
    
    Help text on both scripts spells out the supported invocations:
    
      viewvesta ctrlg.foo.toml          (canonical)
      viewvesta ctrls.foo               (legacy structure-only)
      viewvesta ctrl.foo                (legacy text ctrl)
      viewvesta POSCAR_foo.vasp         (pre-made POSCAR)
    
    Verified in MATERIALS/NiO/ (alat=7.88 Bohr -> ALAT=4.16991 A; 4
    sites Niup/Nidn/O/O -> three POSCAR atom blocks of 1/1/2). All
    three input forms produce a 139-byte POSCAR with matching numerics.

--- StructureTool/ctrl2vasp.py
+++ StructureTool/ctrl2vasp.py
@@ -1,30 +1,43 @@
 #!/usr/bin/env python3
 import re,os,sys,decimal
 from math import *
-#rootpath = os.path.abspath(os.path.dirname(sys.argv[0]))
-#sys.path.append(rootpath+'/convert') # Add path rootpath/convert to read lmf2vasp.py
-#import lmf2vasp
 import convctrl
 argvs = sys.argv
 argc = len(argvs)
-#print
-#print 'args= ',argvs,argc
 
 if (argc != 2 or '--help' in argvs): # help
-	print( ' == Convert ctrl file(input file of lmf) to VASP POSCAR ==')
-	print( '    usage: ctrl2vasp ctrl.foo ')
-	print( '           (ctrls.foo instead)' )
-	print( '           Then we have POSCAR_foo.vasp  (now for Cartesian only)   ')
+	print( ' == Convert ctrl* / ctrlg.*.toml file (input file of lmf) to VASP POSCAR ==')
+	print( '    usage: ctrl2vasp ctrlg.foo.toml          (canonical, post-2026-05)')
+	print( '           ctrl2vasp ctrls.foo               (legacy structure-only)')
+	print( '           ctrl2vasp ctrl.foo                (legacy text ctrl)')
+	print( '           Then we have POSCAR_foo.vasp  (Cartesian only)')
 	sys.exit(-1)
-#if ('--Cartesian' in argvs): 
-#       coordinates='Cartesian'   
-#elif ('--Direct' in argvs): 
-#	coordinates='Direct'   
-#else:
-#	print ' You need to set --Cartesian or --Direct.' 
-#	sys.exit(-1)
-coordinates='Cartesian'   
+coordinates='Cartesian'
+angstrom = 0.529177
 
+inp = argvs[1]
+
+# canonical TOML: ctrlg.<sname>.toml  -- parse with tomllib and feed the
+# same (ALAT, PLAT_list, atomlist) tuple shape convctrl.savefile() wants.
+if inp.startswith('ctrlg.') and inp.endswith('.toml'):
+	ext = inp[len('ctrlg.'):-len('.toml')]
+	titleinput = 'POSCAR_' + ext
+	import tomllib
+	with open(inp, 'rb') as f:
+		d = tomllib.load(f)
+	# struc.alat is in Bohr; convert to angstrom to match savefile output.
+	ALAT = d['struc']['alat'] * angstrom
+	# struc.plat is a 3x3 nested list -> flatten to 9 floats.
+	PLAT_list = [v for row in d['struc']['plat'] for v in row]
+	# [[site]] array -> [[name, x, y, z], ...].  savefile() groups
+	# consecutive same-name entries into one POSCAR atom-type block,
+	# matching the original ctrl-text behaviour.
+	atomlist = [[s['atom'], *s['pos']] for s in d['site']]
+	convctrl.savefile(ALAT, PLAT_list, atomlist, titleinput, coordinates)
+	print(' OK! we have ', titleinput)
+	sys.exit(0)
+
+# legacy text ctrl / ctrls path
 for ix in argvs:  #Get extensions for ctrl.ext or ctrls.ext
         print(ix)
         if 'ctrl.' in ix:
@@ -36,7 +49,7 @@ for ix in argvs:  #Get extensions for ctrl.ext or ctrls.ext
 
 titleinput = 'POSCAR_'+ext
 
-openfile = open(argvs[1]).read().split('\n') 
+openfile = open(argvs[1]).read().split('\n')
 perfectopen = convctrl.fileopen(openfile)
 variable_val = convctrl.constlist(openfile)
 print ('defined variables are:',variable_val)
@@ -49,34 +62,22 @@ variable_name = convctrl.keywordname(variable_val)
 
 ALATone = convctrl.alat(perfectopen,variable_name)
 ALATone[0][1] = eval(ALATone[0][1])
-angstrom = 0.529177
 ALAT = ALATone[0][1]*angstrom # ALAT in angstrom
 PLAT_list = convctrl.plat(perfectopen,variable_name)
-#print 'plat=',PLAT_list
 
 for line_ing in range(len(PLAT_list)):
 	PLAT_list[line_ing] = re.sub('/','/1.0/',PLAT_list[line_ing]) # this trick replace 5/3 with 5/1.0/3. In python 2 this gives difference (5/3=1 in python2).
 	PLAT_list[line_ing] = eval(PLAT_list[line_ing])
 
 atomlist = convctrl.atom(perfectopen,variable_name)
-#print atomlist
 for line_all in range(len(atomlist)):
         for line_each in range(1,4): # / is replaced by /1.0/ to avoid "integer division" in python2.x
                 atomlist[line_all][line_each] = re.sub('/','/1.0/',atomlist[line_all][line_each])
                 atomlist[line_all][line_each] = eval(atomlist[line_all][line_each])
-		#print atomlist[line_all][line_each]
-		#print "coordinates=",coordinates
                 if coordinates=='Cartesian' :
                         atomlist[line_all][line_each] = atomlist[line_all][line_each] # *ALAT new VASP
-                        #		elif coordinates=='Direct' :
-                        #			atomlist[line_all][line_each] = atomlist[line_all][line_each]
-                        #		print atomlist[line_all][line_each]
 
 
 savefile = convctrl.savefile(ALAT,PLAT_list,atomlist,titleinput,coordinates)
 
 print (' OK! we have ' ,titleinput)
-
-
-
-
